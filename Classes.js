f2482afe9d00399cffbd8ef34a4e3ab3c11e5cb3
Eric <ericmarcalmusic@outlook.com>
2024-03-17 20:59:42 -0300

    classe

---
+++ Classes.js
@@ -0,0 +1,16 @@
+let nome = Eric 
+let idade = 28 
+class heroi {
+constructor (Mago, Guerreiro, Monge, Ninja){
+this.Mago = Mago
+this.Guerreiro = Guerreiro
+this.Monge = Monge
+this.Ninja = Ninja
+}
+atacar(){
+  console.log("O" + constructor +"atacou usando" + ataque)
+  }
+}
+let ataque = new heroi ("magia", "espada", "artes marciais", "shuriken")
+
+  ataque.atacar()
